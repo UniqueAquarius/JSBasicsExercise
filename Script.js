a111ea8a2d2b6fa9f8a0fe4e3ace10261dcea03d
Theresa Dillon <97997820+UniqueAquarius@users.noreply.github.com>
2022-03-03 21:06:02 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,56 @@
+console.log(8);
+
+/*1. Use a console log to have your favorite number display in the console/terminal
+2. Use a comment to type out the 5 main data types (AKA Primitives)
+3. Use a comment to type out the 6 operator symbols
+4. Use modulus to get a remainder of 1 from the number 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
+5. Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
+6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+7. Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
+8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+// +
+// -
+// *
+// /
+// %
+// **
+
+console.log(5%4); // 1
+console.log(5%2); // 1
+
+console.log('I\'m Mary Poppins Y\'all');
+
+console.log("Hawk" + "eye");
+
+console.log("Theresa".length +"Dillon".length);// 11
+
+console.log("Avengers: Endgame"[16]);
+
+// BONUS!!! 
+
+console.log("\\"); // \
+
+console.log(5 ** 4 -9); //616
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length); // 0
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+let m = 25;
+let c = 4;
+let u = m+c;
+console.log(u);
